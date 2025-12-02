b86e77aca9cf603a3b0bc30d9c3d4f41006e0e92
José Santizo <jmsantizo2003@gmail.com>
2025-12-02 13:36:49 -0600

    Creacion del Proyecto 2.0

---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// TODO: Configurar Entity Framework y MySQL
+// TODO: Configurar servicios personalizados
+// TODO: Configurar repositorios
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
